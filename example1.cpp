b1d5831583b06af373f9a65f10eba6becff62fe9
Para-devl <vtpatil2018@gmail.com>
2026-08-10 22:04:20 +0530

    example

---
+++ example1.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& arr) {
+    vector<vector<int>> result;
+    sort(arr.begin(), arr.end());
+
+    for(int i=0;i<arr.size()-2;i++){
+        if(i>0 && arr[i]==arr[i-1]){
+            continue;
+        }
+        int l=i+1,r=arr.size()-1;
+        while(l<r){
+            int sum=arr[i]+arr[l]+arr[r];
+            if(sum<0){
+                l++;
+            }
+            else if(sum>0){
+                r--;
+            }
+            else{
+                result.push_back({arr[i],arr[l],arr[r]});
+                l++;
+                r--;
+            }
+        }
+    }
+    return result;
+}
+
+int main() {
+    vector<int> arr = {-4, -1, -1, 0, 1, 2};
+
+    vector<vector<int>> ans = threeSum(arr);
+
+    for (auto &triplet : ans) {
+        cout << "{ ";
+        for (int num : triplet) {
+            cout << num << " ";
+        }
+        cout << "}" << endl;
+    }
+
+    return 0;
+}
